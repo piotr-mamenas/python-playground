b7ca083d4cf8e9a3bd58fa62a3d2e4aabd7295b7
Piotr Mamenas <piotr.mamenas@gmail.com>
2017-12-28 12:08:55 +0100

    Small playing around with dataframes in pandas

--- pandas/datafr_ex.py
+++ pandas/datafr_ex.py
@@ -1,8 +1,14 @@
 import numpy as np;
 import pandas as pd;
+from datetime import datetime;
 
 df = pd.read_csv('fin_dataset.csv',engine='python');
 
 print(df.ix[2]);
 
 print(df.columns);
+
+df['Timestamp'] = df.apply(lambda df: datetime.strptime(df['Timestamp'],'%Y-%m-%d'), axis=1);
+
+#matrix = df.as_matrix();
+#print(matrix);
